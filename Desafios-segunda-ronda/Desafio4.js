3fa2bffce9df20dd41d826753e1894aebbe0b9de
Emmanuel Hernández <43358285+EmmanuelHdz01@users.noreply.github.com>
2024-02-09 09:29:18 -0600

    Se saco promedio de los 3  numeros

--- Desafios-segunda-ronda/Desafio4.js
+++ Desafios-segunda-ronda/Desafio4.js
@@ -1,5 +1,5 @@
 function numerosTriples (numero1, numero2, numero3) {
-    console.log(`Tus numeros son ${numero1}, ${numero2} y ${numero3}`);
+    console.log(`Tus numeros son ${numero1}, ${numero2} y ${numero3}, su promedio es ${(numero1 + numero2 + numero3) / 3}`);
 }
 
 numerosTriples(2, 4, 6);
