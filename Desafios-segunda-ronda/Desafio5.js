25fa74b234341fdb9c73685996718fea18d540a3
Emmanuel Hernández <43358285+EmmanuelHdz01@users.noreply.github.com>
2024-02-09 09:17:00 -0600

    Desafio 5

---
+++ Desafios-segunda-ronda/Desafio5.js
@@ -0,0 +1,9 @@
+function mayorOMenor (num1, num2) {
+    if (num1 < num2){
+        console.log(`El numero ${num2} es mayor que el numero ${num1}`);
+    } else {
+        console.log(`El numero ${num1} es mayor que el numero ${num2}`);
+    }
+}
+
+mayorOMenor (5,  10);
