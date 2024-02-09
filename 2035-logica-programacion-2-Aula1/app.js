c454332dde21b6a048ff4e5c2ba5c42df9aac844
Emmanuel Hernández <43358285+EmmanuelHdz01@users.noreply.github.com>
2024-02-09 15:04:43 -0600

    Actualizacion If
    Se agrego condiciones para el numero secreto y se agrego para cambiar la palabra de intento a intentos.

--- 2035-logica-programacion-2-Aula1/app.js
+++ 2035-logica-programacion-2-Aula1/app.js
@@ -1,4 +1,7 @@
-let numeroSecreto = generarNumeroSecreto;
+let numeroSecreto = generarNumeroSecreto();
+let intentos = 1;
+
+console.log(numeroSecreto);
 
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
@@ -8,11 +11,18 @@ function asignarTextoElemento(elemento, texto) {
 
 function verificarIntento() {
     let numeroDeUsuario = parseInt(document.getElementById('valorDeUsuario').value);
-    console.log(typeof(numeroDeUsuario));
-    console.log(numeroSecreto);
-    console.log(typeof(numeroSecreto));
-    console.log(numeroDeUsuario);
     console.log(numeroDeUsuario == numeroSecreto);
+
+    console.log(intentos);
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('p', `Felicidades acertaste el numero secreto en ${intentos} ${(intentos === 1 ) ? 'intento' : 'intentos'}`);
+    } else if (numeroDeUsuario > numeroSecreto) {
+        asignarTextoElemento('p', 'El numero secreto es menor');
+    } else {
+        asignarTextoElemento('p', 'El numero secreto es mayor');
+    }
+
+    intentos++;
 
     return;
 }
