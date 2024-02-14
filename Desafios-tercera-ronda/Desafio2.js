5495959f696292e497d3db6655643e35268f5848
Emmanuel Hernández <43358285+EmmanuelHdz01@users.noreply.github.com>
2024-02-14 14:56:35 -0600

    Desafio 2 Factorial

---
+++ Desafios-tercera-ronda/Desafio2.js
@@ -0,0 +1,28 @@
+let numeroUsuario = 0, contador = 0, factorial = 1;
+
+function calcularFactorial() {
+    // Se ontiene el numero ingresado por el usuario
+    numeroUsuario = parseInt(document.getElementById('factorialUsuario').value);
+
+    factorialUsuario(numeroUsuario);
+
+    // Prueba de dato obtenido
+    // console.log(numeroUsuario);
+}
+
+function factorialUsuario(n) {
+    // numeroUsuario = numeroUsuario!;
+    if (n != 0) {
+        for (contador = 1; contador < n; contador++) {
+
+            factorial = n;
+            factorial = factorial * contador;
+            factorial = factorial +factorial;
+            console.log(factorial);
+        }
+    } else {
+        console.log('El factorial de 0 es 0');
+    }
+
+    // console.log(factorial);
+}
