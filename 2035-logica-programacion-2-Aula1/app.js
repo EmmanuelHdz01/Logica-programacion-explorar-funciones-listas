c9e62f3af2d87c122881b8caca737fd3bf014555
Emmanuel Hernández <43358285+EmmanuelHdz01@users.noreply.github.com>
2024-02-16 17:09:17 -0600

    Modificacion de arreglos

--- 2035-logica-programacion-2-Aula1/app.js
+++ 2035-logica-programacion-2-Aula1/app.js
@@ -1,6 +1,7 @@
 let numeroSecreto = 0;
 let intentos = 0;
 let listaNumeroSorteados = [];
+let numeroMaximo = 10;
 
 console.log(numeroSecreto);
 
@@ -23,16 +24,16 @@ function verificarIntento() {
 
         if (numeroDeUsuario > numeroSecreto) {
             asignarTextoElemento('p', 'El numero secreto es menor');
-            
+
         } else {
             asignarTextoElemento('p', 'El numero secreto es mayor');
-            
+
         }
         limpiarCaja();
         intentos++;
     }
 
-    
+
 
     return;
 }
@@ -42,25 +43,32 @@ function limpiarCaja() {
 }
 
 function generarNumeroSecreto() {
-    let numeroGenerado = Math.floor(Math.random() * 10) + 1;
-    // Si el numero generado esta incluido en la lista 
+    let numeroGenerado = Math.floor(Math.random() * numeroMaximo) + 1;
 
     console.log(numeroGenerado);
     console.log(listaNumeroSorteados);
-    if(listaNumeroSorteados.includes(numeroGenerado)) {
-        return generarNumeroSecreto;
+
+    //Si ya sorteamos todos los numeros
+    if (listaNumeroSorteados.length == numeroMaximo) {
+        asignarTextoElemento(`p`, `Ya se sortearon todos los numeros posibles`);
     } else {
-        listaNumeroSorteados.push(numeroGenerado);
-        return numeroGenerado;
+        // Si el numero generado esta incluido en la lista
+        if (listaNumeroSorteados.includes(numeroGenerado)) {
+            return generarNumeroSecreto();
+        }
+        else {
+            listaNumeroSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
     }
 }
 
 function condicionesIniciales() {
     asignarTextoElemento('h1', 'Juego del numero secreto');
-    asignarTextoElemento('p', 'Indica un numero del 1 al 10');
+    asignarTextoElemento('p', `Indica un numero del 1 al ${numeroMaximo}`);
     numeroSecreto = generarNumeroSecreto();
     intentos = 1;
-    
+
 }
 
 function reiniciarjuego() {
